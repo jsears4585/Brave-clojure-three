6cdd2497993d95af78a8867b0b878a66565f0291
Jason Sears <jsears4585@gmail.com>
2017-09-23 15:56:27 -0400

    first commit

---
+++ src/brave_clojure_three/core.clj
@@ -0,0 +1,7 @@
+(ns brave-clojure-three.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println (str "It was the panda " "in the library " "with a dust buster")))
---
+++ test/brave_clojure_three/core_test.clj
@@ -0,0 +1,7 @@
+(ns brave-clojure-three.core-test
+  (:require [clojure.test :refer :all]
+            [brave-clojure-three.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
